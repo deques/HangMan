1b3907f0ceb2a65ebd298f77f4782d2add15136f
Wai Wong <daomen@gmail.com>
2025-02-07 10:18:10 +0100

    Use mongoDB to fetch random word

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-from pathlib import Path
-import json, random
-import hangy
-
-words_data = Path("data") / "random_words.json"
-
-# Read the file
-with open(words_data, "r") as file:
-    words = json.load(file)
-
-play = "y"
-while (play.lower() == "y"):
-  word = random.choice(words)
-  hangy.hangman(word.lower())
-  play = input("Play again? Y/N ").lower()
-=======
 import hangy, random_word
 
 play = "y"
@@ -28,4 +11,3 @@ while (play.lower() == "y"):
   play = input("Play again? Y/N ").lower()
 
 
->>>>>>> MongoDB
